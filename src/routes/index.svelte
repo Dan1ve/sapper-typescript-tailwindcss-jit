566006b8b21fd5c32c2210bc8a87d057e01f0fc6
Daniel Veihelmann <veihelmann@cqse.eu>
2021-03-15 21:36:55 +0100

    Big bang 💥

---
+++ src/routes/index.svelte
@@ -0,0 +1,49 @@
+<script>
+  import successkid from 'images/successkid.jpg';
+</script>
+
+<svelte:head>
+  <title>Sapper project template</title>
+</svelte:head>
+
+<h1 class="text-red-500 transform translate-y-[-20px]">Great success!</h1>
+
+<figure>
+  <img alt="Success Kid" src={successkid} />
+  <figcaption>Have fun with Sapper!</figcaption>
+</figure>
+
+<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+
+<style>
+  h1,
+  figure,
+  p {
+    text-align: center;
+    margin: 0 auto;
+  }
+
+  h1 {
+    @apply text-xl font-mono;
+  }
+
+  figure {
+    margin: 0 0 1em 0;
+  }
+
+  img {
+    width: 100%;
+    max-width: 400px;
+    margin: 0 0 1em 0;
+  }
+
+  p {
+    margin: 1em auto;
+  }
+
+  @media (min-width: 480px) {
+    h1 {
+      font-size: 4em;
+    }
+  }
+</style>
